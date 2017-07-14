7f0109cc43dad10c92dfb53cb240f0489b5f2f63
Eligio Cachón Menéndez <ecachon@gmail.com>
2017-07-14 16:09:36 -0500

    Se agregaron todas las busquedas
    No funciona búsquedas por nombre

--- PRUEBAS/prueba_fechas.php
+++ PRUEBAS/prueba_fechas.php
@@ -2,10 +2,38 @@
 require_once '../config.php';
 require_once '../funciones.php';
 $where = "";
-if (!empty($_POST['from'])){
-	$fechai = strtotime($_POST['from']) * 1000;
-	$where = " where ".$fechai." <= start";
+$cd1 = $_POST['from'];
+$cd2 = $_POST['to'];
+$cd3 = $_POST['departamento'];
+
+if ((!empty($cd1)) or (!empty($cd3))){
+	$where = ' where ';
+	if (!empty($cd1)) {
+		$fechai = strtotime($_POST['from']) * 1000;
+		$where = $where.$fechai." >= start ";
+		if (!empty($cd2)) {
+			$fechaf = strtotime($_POST['to']) * 1000;
+			$where = $where."and ".$fechaf." <= end ";
+		}
+		if (!empty($cd3)){
+			$where =$where."and responsable_nombre LIKE %".$cd3."%";
+		}
+	}
+	if (!empty($cd3)){
+		$where = $where."responsable_nombre LIKE %".$cd3."%";
+	}
 }
+
+
+
+
+// if (!empty($_POST['from'])){
+// 	$fechai = strtotime($_POST['from']) * 1000;
+// 	$where = " where ".$fechai." <= start";
+// }
+// if (!empty($_POST['TO'])){
+// 	$fechaf = strtotime($_POST['to']) * 1000;
+//}
 $sql = "SELECT * FROM eventos".$where;
 $result = $conexion->query($sql);
 
@@ -40,19 +68,20 @@ $result = $conexion->query($sql);
 		<div class="row">
 			<form action="prueba_fechas.php" method="post">
 				<div class="col-lg-6">
+					<label for="from">Inicio</label>
 			        <div class='input-group date' id='from'>
 			            <input type='text' id="from" name="from" class="form-control" readonly />
-			            <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+			            <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
 			        </div>
 		        </div>
 
 		        <div class="col-lg-6">
+		        	<label for="to">Fin</label>
 			        <div class='input-group date' id='to'>
 			            <input type='text' id="to" name="to" class="form-control" readonly />
-			            <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+			            <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
 			        </div>
 		        </div>
-
 
 
 		       <script type="text/javascript">
@@ -65,11 +94,26 @@ $result = $conexion->query($sql);
 		            });
 		        });
 				</script>
+
 				<br/>
-				<div class="col-lg-2">
-				<button type="submit" class="btn btn-primary">Buscar</button>
+				<div class="col-lg-6">
+					<label for="departamento">Departamento</label>
+					<div class="input-group" id="departamento">
+					<input type="text" id="departamento" name="departamento" class="form-control">
+					<span class="input-group-addon"><span class="glyphicon glyphicon-pawn"></span></span>
+
+					</div>
+				</div>
+
+				<div class="col-lg-6">
+				<br>
+				<!-- <input type="submit" class="btn btn-primary" value="Buscar"> -->
+				<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> Buscar</button> 
 				</div>
 		    </form>
+	    </div>
+	    <div class="row">
+	    	<?php echo $cd1." ".$cd2." ".$cd3; ?>
 	    </div>
 
 		<div class="row">
